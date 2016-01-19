7a90800a13cfed117438d489d12eeea56922cebf
Michael Schieben <babe@rockitbaby.de>
2016-01-19 12:48:28 +0100

    add dots in off as pattern

--- src/components/Editor/Stage/Renderer.jsx
+++ src/components/Editor/Stage/Renderer.jsx
@@ -3,7 +3,7 @@ import React, {Component, PropTypes} from 'react';
 import {Marker} from './Renderer/Marker';
 import {Lines} from './Renderer/Lines';
 import {Labels, getDefs as getLabelDefs} from './Renderer/Labels';
-import {Grid} from './Renderer/Grid';
+import {Grid, getDefs as getGridDefs} from './Renderer/Grid';
 import {Areas, getDefs as getAreaDefs} from './Renderer/Areas';
 import {Forces} from './Renderer/Forces';
 
@@ -20,8 +20,8 @@ export class Renderer extends Component {
   render() {
     const {
       width, height, fieldSize, gridUnit, skin, normalizeCoordinates,
-      minLengthForArrowsToDisplay, triangleSize, energies, highlights,
-      labels,
+      minLengthForArrowsToDisplay, triangleSize, energies,
+      highlights, labels, lines,
     } = this.props;
 
     let classNames = [];
@@ -32,9 +32,17 @@ export class Renderer extends Component {
     labels.forEach(function(name) {
       classNames.push('label-' + name);
     });
+    lines.forEach(function(name) {
+      classNames.push('line-' + name);
+    });
     let className = classNames.join(' ');
 
-    let defs = getAreaDefs().concat(getLabelDefs());
+    const offsetX = Math.floor(width / 2 - fieldSize / 2) % gridUnit
+    const offsetY = Math.floor(height / 2 - fieldSize / 2) % gridUnit
+
+    let defs = getAreaDefs()
+      .concat(getLabelDefs())
+      .concat(getGridDefs(gridUnit, offsetX, offsetY));
 
     return (
       <svg
@@ -45,13 +53,15 @@ export class Renderer extends Component {
         viewBox={`0 0 ${width} ${height}`} >
         <defs>{defs}</defs>
         { this.props.visibility.grid ?
-          <Grid
-            stageWidth={width}
-            stageHeight={height}
-            gridUnit={gridUnit}
-            skin={skin}
-            dots={this.props.dots}
-            highlights={highlights} />
+          <g>
+            <rect width={this.props.width} height={this.props.height} fill="url(#dots)" />
+              <Grid
+                stageWidth={width}
+                stageHeight={height}
+                gridUnit={gridUnit}
+                skin={skin}
+                dots={this.props.dots}/>
+          </g>
           : null }
         <Marker
           stageWidth={width}
@@ -94,14 +104,15 @@ export class Renderer extends Component {
 
 Renderer.propTypes = {
   energies: Forces.propTypes.energies,
-  width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
   fieldSize: PropTypes.number.isRequired,
   gridUnit: PropTypes.number.isRequired,
   normalizeCoordinates: PropTypes.func.isRequired,
   triangleSize: PropTypes.number.isRequired,
   minLengthForArrowsToDisplay: PropTypes.number.isRequired,
   lables: React.PropTypes.arrayOf(PropTypes.string),
+  lines: React.PropTypes.arrayOf(PropTypes.string),
   visibility: PropTypes.shape({
     forces: PropTypes.bool.isRequired,
     grid: PropTypes.bool.isRequired,
@@ -118,6 +129,8 @@ Renderer.propTypes = {
 };
 
 Renderer.defaultProps = {
+  width: 600,
+  height: 600,
   energies: Forces.defaultProps.energies,
   visibility: {forces: true, labels: true, grid: true},
   highlights: [],
